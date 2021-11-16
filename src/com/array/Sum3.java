aa08887da115de0888c5fa878c93a7723bad12e9
praneshmeher <praneshmeher@gmail.com>
2021-11-16 13:50:55 +0530

    commit

--- src/com/array/Sum3.java
+++ src/com/array/Sum3.java
@@ -1,10 +1,32 @@
 package com.array;
 
+import java.util.Iterator;
+
 public class Sum3 {
+	
+	static void reverseArray(int arr[]) {
+		int temp;
+		int start = 0;
+		int end = arr.length-1;
+		while(start<end) {
+			temp = arr[start];
+			arr[start]=arr[end];
+			arr[end]=temp;
+			start++;
+			end--;
+		}
+	}
+	
+	static void printArray(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[] = {1,2,3,4,5};
+		reverseArray(arr);
+		printArray(arr);
 	}
 
 }
