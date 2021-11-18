151c2951278494d9bbdcff3e509a467daca9cbdc
praneshmeher <praneshmeher@gmail.com>
2021-11-18 11:29:49 +0530

    commit

---
+++ src/com/array/Rotation.java
@@ -0,0 +1,27 @@
+package com.array;
+
+public class Rotation {
+	void rotate(int arr[], int n) {
+		for(int i=0;i<n;i++) {
+			int start = arr[0];
+			for(int j=0;j<arr.length-1;j++) {
+				arr[j]=arr[j+1];
+			}
+			arr[arr.length-1]=start;
+		}
+	}
+	
+	void print(int arr[]) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+	public static void main(String args[]) {
+		Rotation r = new Rotation();
+		int arr[] = {1,2,3,4,5};
+		r.rotate(arr, 2);
+		r.print(arr);
+	}
+
+}
